f5821d843b2cfb3183b36bd04ec6a73edcadd0ec
alin <alinoctavian@protonmail.com>
2020-11-11 09:28:25 +0200

    Variable class

---
+++ oubi/src/Constants.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define END_STATEMENT ";"
---
+++ oubi/src/Variable.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Constants.h"
+
+class Variable {
+    double numValue;
+    std::string strValue;
+    std::vector<Variable> tuple;
+    std::string action;
+    std::string varname;
+public:
+
+
+};
